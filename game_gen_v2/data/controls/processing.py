0522bc8f55cda3602a4ac009321be2b3095f01e8
shahbuland <smatiana1@gmail.com>
2024-10-18 19:48:12 -0400

    Fix mappings to be ascii, fix processing to allow for tapped keys

--- game_gen_v2/data/controls/processing.py
+++ game_gen_v2/data/controls/processing.py
@@ -3,6 +3,15 @@ Processing control CSVs
 """
 
 from .mappings import get_keycode
+from .events import (
+    timestamp,
+    etype,
+    get_key_idx,
+    get_mouse_idx,
+    is_down_press,
+    is_keyboard,
+    is_mouse_button
+)
 
 import pandas as pd
 import torch
@@ -16,25 +25,55 @@ def filter_alike_button_events(events):
     """
     :param events: Events across a singular frame
 
-    Collates button events that occured within a single frame to prioritize the ones that occurred most recently.
+    Collates button events that occurred within a single frame to prioritize the ones that occurred most recently.
+    In order to ensure key down events are captured, we create new event types "KEY_TAP" and "MOUSE_TAP"
+    that are created if a key down event is followed by a key up event for keyboard and mouse respectively.
     """
     key_set = set()
+    mouse_set = set()
+
+    key_down_set = set()
+    mouse_down_set = set()
+    
     indices_to_delete = []
 
     # Iterate through events backwards
     for i, event in reversed(list(enumerate(events))):
-        if event['event_type'] in ['KEYBOARD', 'MOUSE_BUTTON']:
-            key_idx = event['event_args'][0] 
-            
+        # Only care about mouse or keyboard events
+        # In both cases, add down events to X_down_set
+        # Add most recent key alone to X_set
+        # Things that aren't recent are marked for deletion
+        if is_keyboard(event):
+            key_idx = get_key_idx(event)
+            if is_down_press(event):
+                key_down_set.add(key_idx)
             if key_idx not in key_set:
                 key_set.add(key_idx)
             else:
-                # If the key is already in the set, mark this index for deletion
+                indices_to_delete.append(i)
+
+        if is_mouse_button(event):
+            mouse_idx = get_mouse_idx(event)
+            if is_down_press(event):
+                mouse_down_set.add(mouse_idx)
+            if mouse_idx not in mouse_set:
+                mouse_set.add(mouse_idx)
+            else:
                 indices_to_delete.append(i)
     
-    # Delete marked indices from the events list
-    for index in sorted(indices_to_delete, reverse=True):
+    # Deletion
+    for index in sorted(indices_to_delete, reverse = True):
         del events[index]
+    
+    # Key tap events, if key down occured *at all* it should be a X_tap event type
+    for event in events:
+        if not is_down_press(event): # key up
+            if is_keyboard(event):
+                if get_key_idx(event) in key_down_set:
+                    event['event_type'] = 'KEY_TAP'
+            if is_mouse_button(event):
+                if get_mouse_idx(event) in mouse_down_set:
+                    event['event_type'] = 'MOUSE_TAP'
 
     return events
 
@@ -117,24 +156,35 @@ def read_events(df, fps, num_frames = None, num_seconds = None, keys_of_interest
     res = torch.zeros(len(frame_events), len(keys_of_interest)+4, dtype=torch.int16)
     for i in tqdm(range(len(frame_events))):
         for event in frame_events[i]:
-            if event['event_type'] == 'KEYBOARD':
-                #print(event['event_args'][0])
-                #print(get_keycode(event['event_args'][0]))
-                key = get_keycode(event['event_args'][0])
+            # Taps take priority as they should supercede down events
+            if etype(event) == 'KEY_TAP':
+                key = get_keycode(get_key_idx(event))
                 if key in keys_of_interest:
                     key_idx = keys_of_interest.index(key)
-                    if not event['event_args'][1]: # key up
-                        res[i:, key_idx] = 0
-                    elif event['event_args'][1]: # key down
+                    res[i, key_idx] = 1  # Set to 1 only for this frame
+                    if i < (len(frame_events)-1): res[i+1:, key_idx] = 0 # An up was also logged, so set rest to false
+            elif etype(event) == "MOUSE_TAP":
+                if get_mouse_idx(event) == 1: # LMB
+                    res[i,-4] = 1
+                    if i < (len(frame_events)-1): res[i+1:,-4] = 0
+                if get_mouse_idx(event) == 2: # RMB
+                    res[i,-3] = 1
+                    if i < (len(frame_events)-1): res[i+1:,-3] = 0
+            elif etype(event) == 'KEYBOARD':
+                key = get_keycode(get_key_idx(event))
+                if key in keys_of_interest:
+                    key_idx = keys_of_interest.index(key)
+                    if is_down_press(event):  # key down
                         res[i:, key_idx] = 1
-            elif event['event_type'] == 'MOUSE_BUTTON':
-                if event['event_args'][0] == 0: # LMB
-                    res[i:, -4] = 1 if event['event_args'][2] else 0 # 1 if down else 0
-                elif event['event_args'][0] == 1: # RMB
-                    res[i:, -3] = 1 if event['event_args'][-1] else 0
+                    else:  # key up
+                        res[i:, key_idx] = 0
+            elif etype(event) == 'MOUSE_BUTTON':
+                if get_mouse_idx(event) == 1:  # LMB
+                    res[i:, -4] = 1 if is_down_press(event) else 0  # 1 if down else 0
+                elif get_mouse_idx(event) == 2:  # RMB
+                    res[i:, -3] = 1 if is_down_press(event) else 0
             elif event['event_type'] == 'MOUSE_MOVE':
-                res[i, -2] = event['event_args'][0] # dx
-                res[i, -1] = event['event_args'][1] # dy
-    
+                res[i, -2] = event['event_args'][0]  # dx
+                res[i, -1] = event['event_args'][1]  # dy
     #print(res.shape)
     return res

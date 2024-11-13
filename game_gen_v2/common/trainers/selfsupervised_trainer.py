6c6c860072e5e3c3c0ae91f80deea5347ebc7255
shahbuland <smatiana1@gmail.com>
2024-11-12 23:54:12 -0500

    self supervised trainer

---
+++ game_gen_v2/common/trainers/selfsupervised_trainer.py
@@ -0,0 +1,53 @@
+import torch
+import wandb
+
+from .base import BaseTrainer
+from ..utils import Stopwatch
+
+class SimpleSelfSupervisedTrainer(BaseTrainer):
+    def train(self, model, train_loader, val_loader = None, sampler = None):
+        model, train_loader, val_loader, opt, scheduler, _ = self.train_preamble(model, train_loader, val_loader)
+
+        sw = Stopwatch()
+        sw.reset()
+
+        for epoch in range(self.config.epochs):
+            for i, inputs in enumerate(train_loader):
+                with self.accelerator.accumulate(train_loader):
+                    loss, extra = model(inputs)
+                    self.accelerator.backward(loss)
+
+                    if self.accelerator.sync_gradients:
+                        if self.config.grad_clip > 0: self.accelerator.clip_grad_norm_(model.parameters(), self.config.grad_clip)
+                    
+                    opt.step()
+                    if scheduler:
+                        scheduler.step()
+                    opt.zero_grad()
+
+                    if self.accelerator.sync_gradients:
+                        self.total_step_counter += 1
+                        if self.ema is not None:
+                            self.ema.update()
+                    
+                    should = self.get_should()
+                    if should['log']:
+                        wandb_dict = {
+                            'loss' : loss.item(),
+                            "time_per_1k_steps" : sw.hit(self.config.log_interval)
+                        }
+                        if extra is not None:
+                            wandb_dict.update(extra)
+                        if should['sample'] and sampler is not None:
+                            if self.ema is None:
+                                samples = sampler(self.accelerator.unwrap_model(model))
+                            else:
+                                samples = sampler(self.ema.ema_model)
+
+                            wandb_dict.update({
+                                "samples" : samples
+                            }) 
+                        wandb.log(wandb_dict)
+                        sw.reset()
+                    if should['save']:
+                        self.save(self.total_step_counter)
